acc19ebf0e64699ff0b46845fd0fdeb000eedf8a
Davids Chijioke Prince <davids.chijioke@gmail.com>
2021-05-20 16:59:29 +0100

    add extra fields to courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(email: 'cj@gmail.com', password: 'ffffff', password_confirmation: 'ffffff')
+# User.create!(email: 'cj@gmail.com', password: 'ffffff', password_confirmation: 'ffffff')
 
 30.times do
     Course.create!([{
     title: Faker::Educator.course_name,
     description: Faker::TvShows::Suits.quote,
     user_id: User.first.id,
+    short_description: Faker::Quote.famous_last_words,
+    language: Faker::ProgrammingLanguage.name,
+    level: 'Beginner',
+    price: Faker::Number.between(from: 1000, to: 20000)
     }])
 end
