2e2e82c8cf24d300f0e5a44bab65051f367032d3
Davids Chijioke Prince <davids.chijioke@gmail.com>
2021-05-25 12:03:31 +0100

    User role editing

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ rolify
   after_create :assign_default_role
 
   def assign_default_role
-    if User.count = 1
+    if User.count == 1
       self.add_role(:admin) if self.roles.blank?
       self.add_role(:teacher)
       self.add_role(:student)
